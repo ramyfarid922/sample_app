96973dde21efb09b64a937ccb0ad3104859b80d1
ramy farid <ramyfarid922@gmail.com>
2015-08-19 10:18:42 +0200

    Add a StaticPages controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,3 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 end
+
+
+
+
+#/.rvm/gems/ruby-2.0.0-p643/gems/railties-4.2.3/lib/rails/templates/rails/welcome
