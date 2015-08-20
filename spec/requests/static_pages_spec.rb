1bb3ec31eee131228406a92c34f0a0866c2b473d
ramy farid <ramyfarid922@gmail.com>
2015-08-20 10:50:41 +0200

    Adds a wrong test for the about page, adds a route for the about action in the StaticPages controller, creates the html view for the about page, fixes the wrong test for the about page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -32,7 +32,7 @@ describe "Static pages" do
 
 	describe "About page" do
 		it "should have the content 'About Us" do
-			visit '/static_pages/help'
+			visit '/static_pages/about'
 			page.should have_content('About Us')
 		end
 	end
